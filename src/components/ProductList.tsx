295b6e63101d7299f6d51c11828be1d49ad7a670
TomSif <thomas.sifferle.programmer@gmail.com>
2026-04-23 09:11:18 +0200

    fix(list): change key element to direct children

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -4,13 +4,13 @@ import { Product } from "../types/index.ts";
 
 const ProductList = () => {
   return (
-    <main className="flex flex-col p-6 gap-8 w-full ">
+    <main className="flex flex-col p-6 gap-8 w-full">
       <h1 className="text-preset-1 text-rose-900">Desserts</h1>
       <section className="w-full">
         <ul className="grid  md:grid-cols-[repeat(3,1fr)] w-full gap-y-8 gap-x-6 ">
           {data.map((product: Product) => (
             <li key={product.name}>
-              <ProductCard key={product.name} product={product} />
+              <ProductCard product={product} />
             </li>
           ))}
         </ul>
