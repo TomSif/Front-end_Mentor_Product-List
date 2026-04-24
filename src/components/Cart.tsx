30b225e8856d6936cef4006d5f979c488e7cfe5e
TomSif <thomas.sifferle.programmer@gmail.com>
2026-04-24 14:09:31 +0200

    refactor(cart): improve accessibility and semantic HTML

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -14,11 +14,7 @@ const Cart = ({ cartItems, removeItemFromCart }: CartProps) => {
     <section className="bg-white w-full flex flex-col p-6 gap-6 rounded-2xl h-auto">
       <h5 className="text-preset-2 text-red">
         Your Cart
-        <span>
-          {"("}
-          {cartItems.length}
-          {")"}
-        </span>
+        <span>({cartItems.length}) </span>
       </h5>
       <ul className="flex flex-col ">
         {cartItems.map((item: CartItem) => {
@@ -29,25 +25,24 @@ const Cart = ({ cartItems, removeItemFromCart }: CartProps) => {
               key={item.name}
             >
               <h6 className="text-preset-4-bold text-rose-900">{item.name}</h6>
-              <div className="flex justify-between">
-                <div className="flex gap-2 ">
+              <div className="flex justify-between items-center">
+                <p className="flex gap-2">
                   <span className="text-red text-preset-4-bold">
                     {item.quantity}x
                   </span>
                   <span className="text-preset-4 text-rose-500">
                     @ {formatPrice(item.price)}
                   </span>
-                  <span className="text-preset-4-bold text-rose-500">
+                  <strong className="text-preset-4-bold text-rose-500">
                     {formatPrice(itemTotal)}
-                  </span>
-                </div>
+                  </strong>
+                </p>
+
                 <button
                   className="border-2 border-rose-400 rounded-full w-4.5 h-4.5 flex items-center justify-center"
-                  onClick={() => {
-                    removeItemFromCart(item.name);
-                  }}
+                  onClick={() => removeItemFromCart(item.name)}
                   type="button"
-                  aria-label="remove item from cart"
+                  aria-label={`Remove ${item.name} from cart`}
                 >
                   <img src="/assets/images/icon-remove-item.svg" alt="" />
                 </button>
@@ -56,19 +51,20 @@ const Cart = ({ cartItems, removeItemFromCart }: CartProps) => {
           );
         })}
       </ul>
-      <div className="text-preset-4 text-rose-900 flex justify-between items-center ">
-        Order Total{" "}
-        <span className="text-preset-2 text-rose-900">
-          {formatPrice(total)}
-        </span>
-      </div>
+      <dl className="flex justify-between items-center text-rose-900">
+        <dt className="text-preset-4">Order Total</dt>
+        <dd className="text-preset-2">{formatPrice(total)}</dd>
+      </dl>
       <div className="bg-rose-50 rounded-md flex text-preset-4 text-rose-900 p-4 items-center justify-center">
-        <span className="pr-2.5">
-          <img src="/assets/images/icon-carbon-neutral.svg" alt="" />
-        </span>
-        <span>This is a </span>
-        <span className="text-preset-4-bold px-1.5"> carbon-neutral </span>
-        <span> delivery</span>
+        <img
+          src="/assets/images/icon-carbon-neutral.svg"
+          alt=""
+          className="mr-2.5"
+        />
+        <p>
+          This is a <span className="text-preset-4-bold">carbon-neutral</span>{" "}
+          delivery
+        </p>
       </div>
       <button
         className="bg-red text-white text-preset-3 flex items-center justify-center p-4 rounded-full"
