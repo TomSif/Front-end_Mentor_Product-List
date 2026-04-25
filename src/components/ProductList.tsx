f371a0fa2ad6f42531785d12aaec8c7b1e033d13
TomSif <thomas.sifferle.programmer@gmail.com>
2026-04-25 11:58:37 +0200

    refactor(product-list): move main tag on app

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -14,23 +14,21 @@ const ProductList = ({
   data,
 }: ProductListProps) => {
   return (
-    <main className="flex flex-col  gap-8 w-full">
+    <section className="flex flex-col  gap-8 w-full">
       <h1 className="text-preset-1 text-rose-900">Desserts</h1>
-      <section className="w-full">
-        <ul className="grid  md:grid-cols-[repeat(3,1fr)] w-full gap-y-8 gap-x-6 ">
-          {data.map((product: Product) => (
-            <li key={product.name}>
-              <ProductCard
-                product={product}
-                quantity={quantities[product.name] ?? 0}
-                onIncrement={onIncrement}
-                onDecrement={onDecrement}
-              />
-            </li>
-          ))}
-        </ul>
-      </section>
-    </main>
+      <ul className="grid  md:grid-cols-[repeat(3,1fr)] w-full gap-y-8 gap-x-6 ">
+        {data.map((product: Product) => (
+          <li key={product.name}>
+            <ProductCard
+              product={product}
+              quantity={quantities[product.name] ?? 0}
+              onIncrement={onIncrement}
+              onDecrement={onDecrement}
+            />
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 };
 
