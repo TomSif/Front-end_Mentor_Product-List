1a0cec42cbcd2185cb43b3d3330895e0c5d95c45
TomSif <thomas.sifferle.programmer@gmail.com>
2026-04-26 14:19:08 +0200

    fix(card): add border-rose-400 missing around add to cart button

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -45,7 +45,7 @@ function ProductCard({
             }}
             type="button"
             aria-label="Add to cart "
-            className="flex items-center justify-center gap-2 text-preset-4-bold text-rose-900 bg-white rounded-full w-40 py-3 -mt-6  hover:border-red hover:text-red hover:cursor-pointer  border border-transparent"
+            className="flex items-center justify-center gap-2 text-preset-4-bold text-rose-900 bg-white rounded-full border border-rose-400 w-40 py-3 -mt-6  hover:border-red hover:text-red hover:cursor-pointer  "
           >
             <span>
               <img src="/assets/images/icon-add-to-cart.svg" alt="" />
